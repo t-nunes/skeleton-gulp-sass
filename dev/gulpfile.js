4f4c6ff3b23425f17189a55b58347eb51181bb62
Thiago Nunes <thiago_nunes_91@yahoo.com.br>
2016-07-27 21:05:28 -0300

    Working example

--- dev/gulpfile.js
+++ dev/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
+var browserSync = require('browser-sync').create();
 
 var autoprefixerOpts = {browsers: ['last 4 versions'],cascade: false};
 
@@ -11,9 +12,24 @@ gulp.task("scss",function(){
             .pipe( plugins.sass())
             .pipe( plugins.autoprefixer(autoprefixerOpts) ) // prefix the styles
             .pipe( plugins.cleanCss() )                     // minify css
-            .pipe( gulp.dest(cssDir) );
+            .pipe( gulp.dest(cssDir) )
+            .pipe(browserSync.stream());
 });
 
 gulp.task("watch", ['scss'], function(){
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+
+    gulp.watch(scssDir + "**/*.scss", ['scss']);
+    gulp.watch("**/*.html").on('change', browserSync.reload);
+
     return gulp.watch(scssDir + '**/*.scss', ['scss']);     // Watch the main sass and execute the task 'scss'
 })
+
+gulp.task('default', ['watch']);
